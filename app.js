bb659443b6cca81932fcbf731acec46abb2f37f7
Olena <kryvtsovaolena@gmail.com>
2023-05-18 21:40:27 +0300

    hw2

--- app.js
+++ app.js
@@ -1,9 +1,80 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+var age;
+var name1;
+var toggle;
+var empty;
+var notInitialize;
+// let callback = (a: number) => {
+//  return number;
+// };
+age = 50;
+name1 = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+var callback = function (a) {
+    return 100 + a;
+};
+var anything;
+anything = -20;
+anything = "Text";
+anything = {};
+var some;
+some = "Text";
+var str;
+if (typeof some === "string") {
+    str = some;
+}
+var person;
+person = ["Max", 21];
+var Load;
+(function (Load) {
+    Load[Load["LOADING"] = 0] = "LOADING";
+    Load[Load["READY"] = 1] = "READY";
+})(Load || (Load = {}));
+var actiontype = {
+    status: Load.LOADING,
+};
+if (actiontype.status === Load.LOADING) {
+    console.log("Status: ", Load.LOADING);
+}
+if (actiontype.status === Load.READY) {
+    console.log("Status: ", Load.READY);
+}
+var union;
+union = "Title";
+union = 13;
+var literal;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+function customError() {
+    throw new Error("Error");
+}
+var page1 = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+        createAt: new Date("2021-01-01"),
+        updateAt: new Date("2021-05-01"),
+    },
+};
+var page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+};
--- app.ts
+++ app.ts
@@ -1,11 +1,106 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+let age: number;
+let name1: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+// let callback = (a: number) => {
+//  return number;
+// };
+
+age = 50;
+name1 = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+const callback = (a: number) => {
+  return 100 + a;
+};
+
+let anything: any;
+anything = -20;
+anything = "Text";
+anything = {};
+
+let some: unknown;
+some = "Text";
+let str: string;
+if (typeof some === "string") {
+  str = some;
+}
+
+let person: [string, number];
+person = ["Max", 21];
+
+enum Load {
+  LOADING,
+  READY,
+}
+
+const actiontype = {
+  status: Load.LOADING,
+};
+
+if (actiontype.status === Load.LOADING) {
+  console.log("Status: ", Load.LOADING);
+}
+if (actiontype.status === Load.READY) {
+  console.log("Status: ", Load.READY);
+}
+
+let union: string | number;
+union = "Title";
+union = 13;
+
+let literal: "enable " | "disable";
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-});
+function customError(): never {
+  throw new Error("Error");
+}
+
+type DataInfo = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1: DataInfo = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2: DataInfo = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
